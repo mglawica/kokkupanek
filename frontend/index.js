9aff278c5a30cb69edced0602301cdacebc299d7
Paul Colomiets <paul@colomiets.name>
2018-01-17 01:42:06 +0200

    Add stubs for frontend and scheduler

---
+++ frontend/index.js
@@ -0,0 +1,43 @@
+import {createStore, applyMiddleware} from 'redux'
+import {attach} from 'khufu-runtime'
+import {Router} from 'khufu-routing'
+
+import {main} from './main.khufu'
+
+let router = new Router(window);
+let khufu_instance = attach(document.getElementById('app'),
+    main(router, VERSION), {
+    store(reducer, middleware, state) {
+        if(typeof reducer != 'function') {
+            return reducer
+        }
+        let mid = middleware.filter(x => typeof x === 'function')
+        if(DEBUG) {
+            let logger = require('redux-logger')
+            mid.push(logger.createLogger({
+                collapsed: true,
+            }))
+        }
+        let store = createStore(reducer, state, applyMiddleware(...mid))
+        for(var m of middleware) {
+            if(typeof m !== 'function') {
+                if(m.type) {
+                    store.dispatch(m)
+                } else if(DEBUG) {
+                    console.error("Wrong middleware", m)
+                    throw Error("Wrong middleware: " + m)
+                }
+            }
+        }
+        return store
+    }
+})
+
+let unsubscribe = router.subscribe(khufu_instance.queue_render)
+
+if(module.hot) {
+    module.hot.accept()
+    module.hot.dispose(() => {
+        unsubscribe()
+    })
+}
---
+++ frontend/webpack.config.js
@@ -0,0 +1,81 @@
+var webpack = require('webpack')
+var MinifyPlugin = require('babel-minify-webpack-plugin')
+var DEV = process.env['NODE_ENV'] != 'production';
+module.exports = {
+    context: __dirname,
+    entry: "./index",
+    output: {
+        path: __dirname + "/../public/js",
+        filename: "bundle.js",
+        publicPath: '/common/js/',
+    },
+    module: {
+        loaders: [{
+            test: /\.khufu$/,
+            loaders: ['babel-loader', 'khufu'],
+            exclude: /node_modules/,
+        }, {
+            test: /\.js$/,
+            loaders: ['babel-loader'],
+            exclude: /node_modules/,
+        }],
+    },
+    resolve: {
+        modules: process.env.NODE_PATH.split(':').filter(x => x),
+    },
+    resolveLoader: {
+        mainFields: ["webpackLoader", "main", "browser"],
+        modules: process.env.NODE_PATH.split(':').filter(x => x),
+    },
+    devServer: {
+        contentBase: '..',
+        proxy: {
+            '/v1/*': {
+                target: 'http://localhost:8379',
+                secure: false,
+            },
+            '/common/css/*': {
+                target: 'http://localhost:8379',
+                secure: false,
+            },
+            '/common/fonts/*': {
+                target: 'http://localhost:8379',
+                secure: false,
+            },
+        },
+        publicPath: '/common/js/',
+        hot: true,
+        historyApiFallback: {
+            index: 'public/index.html',
+        },
+    },
+    plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                khufu: {
+                    static_attrs: !DEV,
+                },
+                babel: {
+                    "plugins": [
+                        "transform-strict-mode",
+                        "transform-object-rest-spread",
+                        "transform-es2015-block-scoping",
+                        "transform-es2015-parameters",
+                        "transform-es2015-destructuring",
+                        "transform-es2015-arrow-functions",
+                        "transform-es2015-block-scoped-functions",
+                    ],
+                },
+            }
+        }),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            VERSION: JSON.stringify("v0.9.13"),
+            "process.env.NODE_ENV": JSON.stringify(process.env['NODE_ENV']),
+            DEBUG: DEV,
+        }),
+    ].concat(DEV ? [] : [
+        new MinifyPlugin({}),
+    ]),
+}
+
