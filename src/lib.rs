9256887a98c85dd10416f212b68360f992aeb4bc
Paul Colomiets <paul@colomiets.name>
2018-02-21 15:54:09 +0200

    Add version sorting utility module

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,11 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use] extern crate serde_derive;
 #[macro_use] extern crate log;
+#[macro_use] extern crate matches;
 
 pub mod logger;
 pub mod input;
 pub mod wrapper;
 pub mod timestamp;
 pub mod lwwset;
+pub mod version;
